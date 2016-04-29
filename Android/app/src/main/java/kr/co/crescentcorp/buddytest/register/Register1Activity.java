cc336e67483693c9396c9b27e34ccda1ab2325aa
진저애비_회사 <gingerAebi@gmaiil.com>
2016-04-29 23:53:00 +0900

    Register

--- Android/app/src/main/java/kr/co/crescentcorp/buddytest/register/Register1Activity.java
+++ Android/app/src/main/java/kr/co/crescentcorp/buddytest/register/Register1Activity.java
@@ -17,10 +17,14 @@ public class Register1Activity extends AppCompatActivity {
     private EditText et_email;
     private EditText et_lastName;
     private EditText et_firstName;
+    private EditText et_password1;
+    private EditText et_password2;
 
     private ImageView iv_email_checker;
     private ImageView iv_lastName_checker;
     private ImageView iv_firstName_checker;
+    private ImageView iv_password1_checker;
+    private ImageView iv_password2_checker;
 
     private RegisterController registerController;
 
@@ -38,14 +42,21 @@ public class Register1Activity extends AppCompatActivity {
         et_email = (EditText) findViewById(R.id.editText_register_email);
         et_lastName = (EditText) findViewById(R.id.editText_register_lastName);
         et_firstName = (EditText) findViewById(R.id.editText_register_firstName);
+        et_password1 = (EditText) findViewById(R.id.editText_register_pw);
+        et_password2 = (EditText) findViewById(R.id.editText_register_pwAgain);
 
         iv_email_checker = (ImageView) findViewById(R.id.imageView_register_check_id);
         iv_lastName_checker = (ImageView) findViewById(R.id.imageView_register_check_lastName);
         iv_firstName_checker = (ImageView) findViewById(R.id.imageView_register_check_firstName);
+        iv_password1_checker = (ImageView) findViewById(R.id.imageView_register_check_pw);
+        iv_password2_checker = (ImageView) findViewById(R.id.imageView_register_check_pwAgain);
 
     }
 
     private void addWatcher() {
+
+        registerController = new RegisterController();
+
         et_email.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -58,7 +69,7 @@ public class Register1Activity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (registerController.emailCheck(s.toString())) {
+                if (registerController.isEmail(s.toString())) {
                     iv_email_checker.setVisibility(View.VISIBLE);
                 } else {
                     iv_email_checker.setVisibility(View.INVISIBLE);
@@ -73,7 +84,7 @@ public class Register1Activity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (registerController.nameCheck(s.toString())) {
+                if (registerController.isName(s.toString())) {
                     iv_lastName_checker.setVisibility(View.VISIBLE);
                 } else {
                     iv_lastName_checker.setVisibility(View.INVISIBLE);
@@ -93,7 +104,7 @@ public class Register1Activity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (registerController.nameCheck(s.toString())) {
+                if (registerController.isName(s.toString())) {
                     iv_firstName_checker.setVisibility(View.VISIBLE);
                 } else {
                     iv_firstName_checker.setVisibility(View.INVISIBLE);
@@ -105,6 +116,34 @@ public class Register1Activity extends AppCompatActivity {
 
             }
         });
+
+        TextWatcher passWordWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String password1 = et_password1.getText().toString();
+                String password2 = et_password2.getText().toString();
+                if (registerController.isPassword(password1,password2)) {
+                    iv_password1_checker.setVisibility(View.VISIBLE);
+                    iv_password2_checker.setVisibility(View.VISIBLE);
+                } else {
+                    iv_password1_checker.setVisibility(View.INVISIBLE);
+                    iv_password2_checker.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        et_password1.addTextChangedListener(passWordWatcher);
+        et_password2.addTextChangedListener(passWordWatcher);
+
     }
 
 }
--- Android/app/src/main/java/kr/co/crescentcorp/buddytest/register/RegisterController.java
+++ Android/app/src/main/java/kr/co/crescentcorp/buddytest/register/RegisterController.java
@@ -1,5 +1,7 @@
 package kr.co.crescentcorp.buddytest.register;
 
+import android.util.Log;
+
 /**
  * Created by JaeBong on 2016-04-17.
  */
@@ -9,7 +11,6 @@ public class RegisterController {
     public boolean nameCheck;
     public boolean passwordCheck;
 
-
     public boolean checkAll() {
         if (emailCheck && nameCheck && passwordCheck) {
             return true;
@@ -17,17 +18,25 @@ public class RegisterController {
         return false;
     }
 
-    public boolean emailCheck(String email) {
+    public boolean isEmail(String email) {
         if (email.contains("@") && email.contains(".")) {
             return emailCheck = true;
         }
         return emailCheck = false;
     }
 
-    public boolean nameCheck(String name) {
+    public boolean isName(String name) {
         if (name.length() > 0) {
             return nameCheck = true;
         }
         return nameCheck = false;
+    }
+
+    public boolean isPassword(String password1, String password2) {
+        Log.i("TEST","password1 : " + password1 + "password2 : " + password2);
+        if(password1.length() > 5 && password1.equals(password2)) {
+            return passwordCheck = true;
+        }
+        return passwordCheck = false;
     }
 }
